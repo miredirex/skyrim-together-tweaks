75693626ac0c6da52e87bc301af479fc229005c9
Daniil Zakharov <miredirex@gmail.com>
2026-02-05 01:55:35 +0300

    Add VectoredExceptionHandler to catch the crash and display a message

--- src/tweaks/UIActivationFix.cpp
+++ src/tweaks/UIActivationFix.cpp
@@ -27,8 +27,41 @@ struct IsDllBlocked
     }
 };
 
+uint64_t g_startTime = 0;
+
+inline ULONGLONG GetSecondsElapsed() { return (GetTickCount64() - g_startTime) / 1000; }
+
+LONG WINAPI UiFixVectoredExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
+{
+    auto isNvCameraPresent = static_cast<bool>(GetModuleHandleW(L"NvCamera64.dll"));
+    auto isEarlyCrash = GetSecondsElapsed() < 10;
+
+    if (!isNvCameraPresent || !isEarlyCrash)
+    {
+        return EXCEPTION_CONTINUE_SEARCH;
+    }
+
+    PEXCEPTION_RECORD pRecord = pExceptionInfo->ExceptionRecord;
+
+    if (pRecord && pRecord->ExceptionCode == EXCEPTION_ACCESS_VIOLATION)
+    {
+        int32_t res = MessageBoxW(
+            NULL,
+            L"Skyrim Together has crashed. Please disable \"Game filters and Photo mode\" in the"
+            " NVIDIA App/GeForce Experience and try again. Photo mode is also known as NVIDIA Ansel or NvCamera64.dll",
+            L"Skyrim Together Tweaks",
+            MB_OK);
+
+        if (res == IDOK)
+            TerminateProcess(GetCurrentProcess(), 1);
+    }
+
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
 void UIActivationFix::Install()
 {
+    g_startTime = GetTickCount64();
     // TODO: TOML setting `bEnableF2Fix`
     // TODO: `FindPatternInST` sometimes hangs, investigate
 
@@ -36,11 +69,21 @@ void UIActivationFix::Install()
 
     uintptr_t addrApplyNvidiaFix = memscan::FindPatternInST(memscan::aob::TiltedOnlineApp_ApplyNvidiaFix);
     if (addrApplyNvidiaFix)
+    {
         trampoline.write_branch<5>(addrApplyNvidiaFix, ApplyNvidiaFix::hook);
+    }
 
     uintptr_t addrIsDllBlocked = memscan::FindPatternInST(memscan::aob::DllBlocklist_IsDllBlocked);
     if (addrIsDllBlocked)
+    {
         trampoline.write_branch<5>(addrIsDllBlocked, IsDllBlocked::hook);
+    }
+
+    // Install the exception handler only if we hooked successfully
+    if (addrApplyNvidiaFix && addrIsDllBlocked)
+    {
+        AddVectoredExceptionHandler(1, UiFixVectoredExceptionHandler);
+    }
 
     spdlog::info("Installed UI activation fix");
 }
