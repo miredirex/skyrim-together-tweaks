0b617513ba505354ed249224a86d4fa6c97568ae
Daniil Zakharov <miredirex@gmail.com>
2026-02-08 22:06:49 +0300

    Update fn signature

--- src/MemoryScan.h
+++ src/MemoryScan.h
@@ -10,7 +10,7 @@ namespace memscan
         const std::string_view DllBlocklist_IsDllBlocked = "48 89 5C 24 08 48 89 6C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 60 48";
         const std::string_view RunTiltedInit_g_appInstanceMov = "48 8B 1D ?? ?? ?? ?? 48 89 3D ?? ?? ?? ?? 48 85 DB";
 
-        const std::string_view DInputHook_Get = "48 83 EC 38 8B 0D ?? ?? ?? ?? 65 48 8B 04 25 58 00 00 00 BA";
+        const std::string_view DInputHook_Get = "48 83 EC 38 B9 68";
         const std::string_view DInputHook_Get_v180 = "40 53 48 83 EC 40 B9";
 
         const std::string_view DInputHook_SetToggleKeys = "48 89 5C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 40 4C 8B F2";
